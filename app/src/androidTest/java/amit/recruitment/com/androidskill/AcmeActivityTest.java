518ced1fac56ba3f30732ac385335fd9ef28f2b3
amit_kumar_saha <amit.saha012@gmail.com>
2018-03-13 14:39:34 -0500

    According to requirments

---
+++ app/src/androidTest/java/amit/recruitment/com/androidskill/AcmeActivityTest.java
@@ -0,0 +1,73 @@
+package amit.recruitment.com.androidskill;
+
+/**
+ * Created by amit on 3/13/18.
+ */
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.widget.TextView;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class AcmeActivityTest extends ActivityInstrumentationTestCase2<AcmeActivity> {
+
+    private AcmeActivity mTestActivity;
+    private TextView mTestFortuneTextview;
+    private FloatingActionButton mFab;
+
+    public AcmeActivityTest() {
+        super(AcmeActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        // Starts the activity under test using
+        // the default Intent with:
+        // action = {@link Intent#ACTION_MAIN}
+        // flags = {@link Intent#FLAG_ACTIVITY_NEW_TASK}
+        // All other fields are null or empty.
+        mTestActivity = getActivity();
+        mTestFortuneTextview = (TextView) mTestActivity
+                .findViewById(R.id.tv_acme_fortune);
+        mFab = (FloatingActionButton) mTestActivity
+                .findViewById(R.id.fab);
+    }
+
+    /**
+     * If this tests fails all other tests are
+     * likely to fail as well.
+     */
+    public void testPreconditions() {
+        // Try to add a message to add context to your assertions.
+        // These messages will be shown if
+        // a tests fails and make it easy to
+        // understand why a test failed
+        assertNotNull("mTestActivity is null", mTestActivity);
+        assertNotNull("mTestFortuneTextview is null", mTestFortuneTextview);
+        assertNotNull("mFab is null", mFab);
+    }
+    public void  testDataFetched(){
+
+        getActivity();
+        Espresso.onView(ViewMatchers.withId(R.id.fab))
+                .perform(ViewActions.click());
+
+        assertNotNull("mTestFortuneTextview content is null after fetching",mTestFortuneTextview.getText());
+
+    }
+
+
+}
---
+++ app/src/main/java/amit/recruitment/com/androidskill/AcmeActivity.java
@@ -0,0 +1,204 @@
+package amit.recruitment.com.androidskill;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.tasomaniac.android.widget.DelayedProgressDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class AcmeActivity extends AppCompatActivity {
+
+
+
+    private final String ACME_BASE_URL = "http://api.acme.international";
+    private final String ACME_API_URL = ACME_BASE_URL+"/fortune";
+    private final String REQUEST_TAG = "AcmeActivity_REQUEST";
+    private final String CONTENT_TAG = "AcmeActivity_FORTUNE_CONTENT";
+    private final String LOADING_TAG = "AcmeActivity_FORTUNE_LOADING";
+
+    private final String APP_TAG = "AndroidSKILL";
+
+
+    private String storedValue=null;
+    private boolean wasLoadingState=false;
+
+    private TextView mFortuneTextview;
+
+    private RequestQueue mQueue=null;
+
+    private DelayedProgressDialog mProgressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acme);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mFortuneTextview = (TextView) findViewById(R.id.tv_acme_fortune);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dispatchRequest();
+            }
+        });
+
+        if(savedInstanceState!=null){
+            storedValue= savedInstanceState.getString(CONTENT_TAG,null);
+            wasLoadingState = savedInstanceState.getBoolean(LOADING_TAG,false);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(storedValue!=null){
+            mFortuneTextview.setText(storedValue);
+
+            if(wasLoadingState){
+                // if it was in a loading state during orientation change, then dispatching again
+                dispatchRequest();
+            }
+
+        }else{
+            // 1st time loading case
+            dispatchRequest();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        if(mFortuneTextview!=null && mFortuneTextview.getText()!=null){
+            // saving the content
+            outState.putCharSequence(CONTENT_TAG,mFortuneTextview.getText());
+        }
+
+        if(mProgressDialog!=null && mProgressDialog.isShowing()){
+            // saving the loading state
+            outState.putBoolean(LOADING_TAG,true);
+        }else{
+            outState.putBoolean(LOADING_TAG,false);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        if(savedInstanceState!=null){
+            String storedVal= savedInstanceState.getString(CONTENT_TAG);
+            if(storedVal!=null){
+                mFortuneTextview.setText(storedVal);
+            }
+
+            boolean wasLoadingState = savedInstanceState.getBoolean(LOADING_TAG,false);
+
+            if(wasLoadingState){
+                dispatchRequest();
+            }
+        }
+
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private void dispatchRequest(){
+
+        if(mQueue==null){
+            mQueue = Volley.newRequestQueue(this);
+        }else{
+            mQueue.cancelAll(REQUEST_TAG);
+        }
+        startLoading();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, ACME_API_URL, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        stopLoading();
+                        StringBuilder stringBuilder = new StringBuilder();
+                        try {
+                            JSONArray fortuneArray = response.getJSONArray("fortune");
+
+                            for(int i=0;i<fortuneArray.length();i++){
+
+                                stringBuilder.append(fortuneArray.getString(i));
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.d(APP_TAG,e.getMessage());
+                            mFortuneTextview.setText("Invalid JSON Structure");
+                        }
+                        mFortuneTextview.setText("Response: " + stringBuilder.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        stopLoading();
+                        // TODO: Handle error
+                        Log.d(APP_TAG,error.getMessage());
+                        mFortuneTextview.setText("Error : "+error.getLocalizedMessage());
+                    }
+                });
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                10000, /* 10 sec timeout policy */
+                0, /*no retry*/
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        jsonObjectRequest.setTag(REQUEST_TAG);
+        mQueue.add(jsonObjectRequest);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mQueue!=null){
+            mQueue.cancelAll(REQUEST_TAG);
+        }
+        stopLoading();
+    }
+
+    private void startLoading(){
+        stopLoading();
+        mProgressDialog = DelayedProgressDialog.make(this,getString(R.string.dialog_title),getString(R.string.dialog_message),true);
+        mProgressDialog.show();
+    }
+
+    private void stopLoading(){
+        if(mProgressDialog!=null){
+            mProgressDialog.dismiss();
+            mProgressDialog=null;
+        }
+    }
+
+}
